add87280e8b448d6d559b21eb36ba69e09c27aa3
Siwat Sirichai <siwat2545@cud55.com>
2020-08-12 21:00:00 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,15 @@
-if [ ! -f /config/* ]; then
-    echo "File not found!"
+echo "Starting Container Initialization Script."
+if [ -z "$(ls -A /config)" ]; then
+   echo "Initializing . . ."
+   cd /config
+   git clone https://github.com/danxfisher/Plex-Discord-Bot.git
+   cd Plex-Discord-Bot
+   npm install
+   node index.js
+   exit 0
+else
+   cd /config/Plex-Discord-Bot
+   npm install
+   node index.js
+   exit 0
 fi
